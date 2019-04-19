aec840054ab4db44700f9a3837b8e796b562a450
HNog <hebertnogueira155@gmail.com>
2019-04-19 12:20:16 -0300

    my first react app

---
+++ build/precache-manifest.bc503b3b5687ebc1ff5314d42468f8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "260e9faf241aa22b967f",
+    "url": "/static/css/main.621cda49.chunk.css"
+  },
+  {
+    "revision": "260e9faf241aa22b967f",
+    "url": "/static/js/main.b9c029de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a1de08bbafff7aed8b4",
+    "url": "/static/css/2.f12da91d.chunk.css"
+  },
+  {
+    "revision": "4a1de08bbafff7aed8b4",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4aef49da6f406ddc7b686c883911cacd",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const SearchBox = ({ seachChange}) => {
+	return (
+		<div className='pa2'>
+			<input 
+				className='pa3 ba b--green bg-lightest-blue'
+				type='search' 
+				placeholder='search robots' 
+				onChange={seachChange}
+			/>
+		</div>
+	);
+}
+
+export default SearchBox;
